059d82a724ec072e101c4af11852e6ce2920a940
rishavdhungel <rishavdhungel3@gmail.com>
2020-07-29 17:23:59 +0545

    Learn_Python3_The_Hard_Way

---
+++ Learn_Python3_The_Hard_Way/ex2.py
@@ -0,0 +1,6 @@
+#An comment, this is so you can read your program later
+#Anything after the # is ignored by python
+print("I could have code like this. ")#and the comment after is igored
+#You can also use a comment ti "disable " or comment out code:
+#print("This will not run")
+print("This will run.")
---
+++ Learn_Python3_The_Hard_Way/ex3.py
@@ -0,0 +1,21 @@
+print("I will now count my chickens:")
+#Just Doing some Maths LOL!
+print("Hens", 25 + 30 / 6)
+print("Roosters", 100 - 25 * 3 % 4)
+
+print("Now I will count the eggs: ")
+print(3 + 34 + 23 - 20 % 10 - 1 / 4)
+print("Is is true that  100 + 500 > 100 + 400?")
+
+print(100 + 500 > 100 + 400)
+
+print("What is 100 + 500?" , 100 + 500)
+print("What is 100 + 400?" , 100 + 400)
+
+print("oh, that's why it's True .")
+
+print("How about some more. ")
+
+print("Is it greater?" , 500 > - 100)
+print("Is it greater or equal?" , 500 >= -100)
+print("IS is less or equal?" , 500 <= -100)
