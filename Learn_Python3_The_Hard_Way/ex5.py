195d896b05ca77ac93e582447d18490a88593caf
rishavdhungel <rishavdhungel3@gmail.com>
2020-07-30 09:14:38 +0545

    tired with print

---
+++ Learn_Python3_The_Hard_Way/ex5.py
@@ -0,0 +1,17 @@
+my_name = "Rishav Dhungel"
+my_age = 20
+my_height = 5.8 *12 #inches
+my_weight = 60 #kg
+my_eyes = "Brown"
+my_teeth = "white"
+my_hair = "Black"
+
+print(f"Let's talk about {my_name}.")
+print(f"He's {my_height} inches tall.")
+print(f"He's {my_weight} kg heavy.")
+print("Actually that's not too heavy.")
+print(f"He's got {my_eyes} eyes and {my_hair} hair.")
+print(f"His teeth are usually {my_teeth} depending on coffee.")
+
+total = my_age + my_height + my_weight
+print(f"If I add {my_age},{my_height}, and {my_weight} I get {total}.")
